fd8d1d804f4a5cf7997f07b3ce320d1828c58782
Samuellucas97 <samuellucas97@ufrn.edu.br>
2020-07-29 20:47:04 -0300

    Adicionando versão do programa com Maven

---
+++ euler/src/main/java/euler/EulerCalculatorTerm.java
@@ -0,0 +1,28 @@
+package euler;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.concurrent.Callable;
+
+public class EulerCalculatorTerm implements Callable<BigDecimal>{
+	private Integer numberOfTerm;
+	
+	public EulerCalculatorTerm(String nameThread, Integer numberOfTerm) {
+		this.numberOfTerm = numberOfTerm;
+	}
+
+	@Override
+	public BigDecimal call() throws Exception {
+		BigInteger factorialResult = factorial(BigInteger.valueOf(numberOfTerm));
+	    System.out.println("Currently active threads are " + Thread.activeCount());
+	    
+	    return BigDecimal.ONE.divide(new BigDecimal(factorialResult), 50, RoundingMode.HALF_EVEN);
+	}
+
+	private BigInteger factorial(BigInteger number) {
+		if (number == BigInteger.ZERO)
+			return BigInteger.ONE;	
+		return number.multiply(factorial(number.subtract(BigInteger.ONE)));
+	}
+}
---
+++ euler/src/main/java/euler/Main.java
@@ -0,0 +1,95 @@
+package euler;
+
+import java.math.BigDecimal;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+	private static void checkArgument(String modeThreadPool) {			
+		if (	(!modeThreadPool.equals("F") && !modeThreadPool.equals("C") && !modeThreadPool.equals("W")) ) {
+			throw new IllegalArgumentException("O segundo parâmetro refere-se ao tipo de thread pool. "
+					+ "As opcoes são 'F' para a fixed, C para  cached e 'W' para a work stealing.");
+		}		
+	}
+	
+	public static void main(String[] args) {
+
+		Integer numberTerms = 0;
+		String modeThreadPool = "";
+		Integer numberThreads = 0;
+		
+		if (args.length != 2 && args.length != 3) {
+			throw new IllegalArgumentException("Há dois parâmetros obrigatórios. O primeiro parâmetro é a quantidade de "
+					+ "termos. O segundo parâmetro a forma de thread pool: fixed (F), cached (C) e work stealing (W)."
+					+ " Caso seja escolhida a forma de thread pool fixed deve acrescentar um "
+					+ "terceiro parâmetro, a quantidade de threads ");	
+		}
+		else {
+			numberTerms = Integer.parseInt(args[0]);
+			modeThreadPool = args[1];
+			
+			if (args.length == 3) 
+				numberThreads = Integer.parseInt(args[2]);
+			
+			checkArgument(modeThreadPool);
+		}
+		
+		if ( modeThreadPool.equals("F") ) {
+			ExecutorService executor = Executors.newFixedThreadPool(numberThreads);
+			BigDecimal euler =  BigDecimal.ONE;
+		      
+			for (int i = 1; i< numberTerms+1; ++i) {
+			 	Callable<BigDecimal> termEulerCalculator = new EulerCalculatorTerm("Thread-" + i, i); 
+				Future<BigDecimal> termEuler = executor.submit(termEulerCalculator);
+				try {
+					//euler += termEuler.get();
+					euler = euler.add(termEuler.get());
+				} catch (InterruptedException | ExecutionException e) {
+					e.printStackTrace();
+				}	
+			}
+			
+			System.out.println("(FIXED) - Result is " + euler);
+			executor.shutdown();
+		}
+		else if (modeThreadPool.equals("C") ) {
+			ExecutorService executor = Executors.newCachedThreadPool();
+			BigDecimal euler =  BigDecimal.ONE;
+		      
+			for (int i = 1; i< numberTerms+1; ++i) {
+			 	Callable<BigDecimal> termEulerCalculator = new EulerCalculatorTerm("Thread-" + i, i); 
+				Future<BigDecimal> termEuler = executor.submit(termEulerCalculator);
+				try {
+					//euler += termEuler.get();
+					euler = euler.add(termEuler.get());
+				} catch (InterruptedException | ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			System.out.println("(CACHED) - Result is " + euler);
+			executor.shutdown();
+		}
+		else {
+			ExecutorService executor = Executors.newWorkStealingPool(Runtime.getRuntime().availableProcessors());
+			BigDecimal euler =  BigDecimal.ONE;
+		      
+			for (int i = 1; i< numberTerms+1; ++i) {
+			 	Callable<BigDecimal> termEulerCalculator = new EulerCalculatorTerm("Thread-" + i, i); 
+				Future<BigDecimal> termEuler = executor.submit(termEulerCalculator);
+				try {
+					//euler += termEuler.get();
+					euler = euler.add(termEuler.get());
+			    } catch (InterruptedException | ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			System.out.println("(CACHED) - Result is " + euler);
+			executor.shutdown();
+		}	
+	}
+}
